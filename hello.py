ba47c4ed763580f3d90cb4260f8c6e37d2150f13
Scott <scd.fien@gmail.com>
2024-11-09 07:06:12 -0500

    added python file to make sure everything is working properly

---
+++ hello.py
@@ -0,0 +1,17 @@
+print("Hello, SpaceX Data Engineer")
+
+# Integer
+launch_count = 10
+
+# Flaot
+success_rate = 0.95
+
+# String
+rocket_name = 'Falcon 9'
+
+# Boolean
+is_active = True
+
+total_launches = launch_count * 5
+
+print(total_launches)
